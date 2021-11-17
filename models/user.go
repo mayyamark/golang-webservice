2abff269394b0ac8dc16cf7545e9d08cd7a76595
Mayya Markova <mayya.markova@gmail.com>
2021-11-17 17:12:35 +0200

    Add behavior

--- models/user.go
+++ models/user.go
@@ -5,3 +5,21 @@ type User struct {
 	FirstName string
 	LastName  string
 }
+
+var (
+	users  []*User
+	nextID = 1
+)
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+
+	users = append(users, &u)
+
+	return u, nil
+}
