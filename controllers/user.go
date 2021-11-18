f9b594b2ec88cb4faddb838f5a8271c3cc7bca23
Mayya Markova <mayya.markova@gmail.com>
2021-11-18 17:21:00 +0200

    Add a construction function

--- controllers/user.go
+++ controllers/user.go
@@ -1,11 +1,24 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type userController struct{}
+type userController struct {
+	userIDPattern *regexp.Regexp // to match the invomming http req path
+}
 
 // bind the function to the userController
 func (uc userController) serverHTTP(w http.ResponseWriter, r *http.Request) {
 	// type conversion: byte to string
 	w.Write([]byte("Hello from the User Controller!"))
+}
+
+// a construction function
+func newUserController() *userController {
+	// create a userController and return the address of it
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
 }
