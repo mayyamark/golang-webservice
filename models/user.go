5f036b16bebf1fc6b9b82272269e9cd3b4ae0039
Mayya Markova <mayya.markova@gmail.com>
2021-11-19 17:41:40 +0200

    Add more funcs in the models layer

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,10 +21,51 @@ func GetUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("new User must not include id or it must be set")
+	}
+
 	u.ID = nextID
 	nextID++
 
 	users = append(users, &u)
 
 	return u, nil
+}
+
+func GetUserById(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+
+	// Errorf from fmt package allows us to send a formatted string as an error
+	return User{}, fmt.Errorf("user with ID '%v' not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("user with ID '%v' not found", u.ID)
+}
+
+func RemoveUserById(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			// splice the users => create a new slice:
+			// - users[:i]: from index 1 to the passed user's index
+			// - users[i+1:]...: from the passed user's index + 1 to the end of the slice
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("user with ID '%v' not found", id)
+
 }
